cf8cc4b2c3b4d61cee89e77c82fd29d576dbe186
devSirNaelC7 <sirnaelc7@gmail.com>
2022-11-06 11:38:16 -0300

    feat: inserting data in home and recipe templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from utils.recipes.factory import make_recipe
+
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name':'Naelson Fernandes',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+    })
